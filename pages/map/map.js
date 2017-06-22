6fe76abbb0e5004eec671b06027a848988e42f60
FloreU <844402373@qq.com>
2017-06-23 00:17:00 +0800

    +333

--- pages/map/map.js
+++ pages/map/map.js
@@ -5,6 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
+    array: ["哲学学院", "国学院", "文学院", "外国语言文学学院", "新闻与传播学院", "艺术学院", "历史学院", "经济与管理学院", "法学院", "马克思主义学院", "社会学系", "政治与公共管理学院", "教育科学研究院", "信息管理学院", "国际教育学院", "数学与统计学院", "物理科学与技术学院", "化学与分子科学学院", "生命科学学院", "资源与环境科学学院", "高等研究院", "动力与机械学院", "电气工程学院", "城市设计学院", "土木建筑工程学院", "水利水电学院", "电子信息学院", "计算机学院", "国际软件学院", "测绘学院", "遥感信息工程学院", "印刷与包装系", "医学研究院", "基础医学院", , "健康学院", "第一临床医学院", "第二临床医学院", "口腔医学院", "药学院", "医学职业技术学院", "医学部机关"],
+    index: 0,
     Height: 0,
     scale: 16,
     latitude: "",
@@ -12,11 +14,16 @@ Page({
     markers: [],
     controls: [],
     circles: [],
-    tapColor: ["#1472e0", "#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0"],
-    tapSize: [34, 30, 30, 30, 30, 30, 30, 30],
+    tapColor: ["#1472e0", "#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0"],
+    tapSize: [34, 30, 30, 30, 30, 30, 30, 30, 30],
     view:{
       Height: 755
-    }
+    },
+    windowHeight: 603,
+    windowWidth: 375,
+    mylocLat: "",
+    mylocLong: "",
+    mapCtx: {}
   },
   
   /**
@@ -29,13 +36,15 @@ Page({
     var DXHL=[];
     var fileData = require('../../utils/data.js');
     _this.template = require('../../utils/template.js');
-    _this.setData({
-      controls: _this.template.leftInitControls()
-    });
     wx.getSystemInfo({
       success: function (res) {
         //设置map高度，根据当前设备宽高满屏显示
         _this.setData({
+          controls: _this.template.leftInitControls(res.windowHeight, res.windowWidth)
+        });
+        _this.setData({
+          windowHeight: res.windowHeight,
+          windowWidth: res.windowWidth,
           view: {
             Height: res.windowHeight - res.windowWidth / 750 * 80,
           }
@@ -47,6 +56,7 @@ Page({
       longitude: 114.36216115951538,
       markers: fileData.JIUYE()
     });
+    _this.data.mapCtx = wx.createMapContext('map');
     // wx.getLocation({
     //   type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
     //   success: function (res) {
@@ -118,41 +128,65 @@ Page({
       url: '../detail/detail?id=' + id
     })
   },
-
-  controltap(e) {
+  bindPickerChange: function (e) {
     var that = this;
     var fileData = require('../../utils/data.js');
+    var school = fileData.SCHOOLS()[e.detail.value];
+    that.setData({
+      index: e.detail.value,
+      markers: [school],
+      latitude: school.latitude,
+      longitude: school.longitude,   
+    });
+  },
+  controltap(e) {
+    var that = this;
+    var H = that.data.windowHeight;
+    var W = that.data.windowWidth;
+    var fileData = require('../../utils/data.js');
     if (e.controlId === 1){
-      wx.getLocation({
-        type: 'gcj02',
-        success: function (res) {
-          that.setData({
-            latitude: res.latitude,
-            longitude: res.longitude,
-            scale: 16,
-          });
-        }
-      })
+      // wx.getLocation({
+      //   type: 'gcj02',
+      //   success: function (res) {
+      //     that.setData({
+      //       mylocLat: res.latitude,
+      //       mylocLong: res.longitude
+      //     });
+      //   },
+      //   fail: function (res){
+      //     console.log("fail");
+      //   },
+      //   complete: function (res) {
+      //     var lat = that.data.mylocLat ;
+      //     var long = that.data.mylocLong ;
+      //     that.setData({
+      //       latitude: lat,
+      //       longitude: long,
+      //       scale: 16,
+      //     });
+      //   }
+      // })
+      that.data.mapCtx.moveToLocation();
     };
     if (e.controlId === 11) {
       that.setData({
         markers: fileData.GXB_M(),
         polyline: fileData.GXB_L(),
-        controls: that.template.buslineClickControls1()
+        controls: that.template.buslineClickControls1(H, W)
       });
     };
     if (e.controlId === 12) {
       that.setData({
         markers: fileData.WLXB_M(),
         polyline: fileData.WLXB_L(),
-        controls: that.template.buslineClickControls2()
+        controls: that.template.buslineClickControls2(H, W)
       });
     };
     if (e.controlId === 13) {
       that.setData({
         markers: fileData.DXH_M(),
         polyline: fileData.DXH_L(),
-        controls: that.template.buslineClickControls3()
+        controls: that.template.buslineClickControls3(H, W)
       });
     };
   },
@@ -162,6 +196,8 @@ Page({
     var that = this;
     var colorArray = ["#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0", "#7BB2F0"];
     var sizeArray = [30, 30, 30, 30, 30, 30, 30, 30];
+    var H = that.data.windowHeight;
+    var W = that.data.windowWidth;
     colorArray[id] = "#1472e0";
     sizeArray[id] = 34;
     that.setData({
@@ -170,24 +206,24 @@ Page({
     });
     var showBusline = function(){
       that.setData({
-        controls: that.template.buslineClickControls3()
+        controls: that.template.buslineClickControls3(H, W)
       });
     };
     var hideBusline = function(){
       that.setData({
-        controls: that.template.leftInitControls(),
+        controls: that.template.leftInitControls(H, W),
         polyline: []
       });
     }
     switch(id){
       case 0: hideBusline(); that.setData({ markers: fileData.JIUYE() }); break;
       case 1: hideBusline(); that.setData({ markers: fileData.BAOGAOTING() }); break;
-      case 2: hideBusline(); that.setData({ markers: fileData.TIYUGUAN() }); break;
-      case 3: hideBusline(); that.setData({ markers: fileData.SCHOOLS() }); break;
-      case 4: hideBusline(); that.setData({ markers: fileData.SCHOOLS_GATE() }); break;
-      case 5: hideBusline(); showBusline(); that.setData({ markers: fileData.DXH_M(), polyline: fileData.DXH_L()}); break;
+      case 5: hideBusline(); that.setData({ markers: fileData.TIYUGUAN() }); break;
+      case 2: hideBusline(); break;
+      case 3: hideBusline(); that.setData({ markers: fileData.SCHOOLS_GATE() }); break;
+      case 4: hideBusline(); showBusline(); that.setData({ markers: fileData.DXH_M(), polyline: fileData.DXH_L()}); break;
       case 6: hideBusline(); that.setData({ markers: fileData.DITIE() }); break;
-      case 7: showBusline(); break;
+      case 7: hideBusline();  break;
     }
   }
 })
--- utils/template.js
+++ utils/template.js
@@ -1,187 +1,63 @@
 module.exports = {
-  buslineInitControls: function(){
-    return [{
-      id: 11,
-      iconPath: '/icon/gong2.png',
+  rightTemplateFactory: function(H, W, id, iconPath, xoff){
+    return {
+      id: id,
+      iconPath: iconPath,
       position: {
-        left: 220,
-        top: 540 - 50,
+        left: W - xoff,
+        top: H - 40 - 40 - 40,
         width: 40,
         height: 40
       },
       clickable: true
-    }, {
-      id: 12,
-      iconPath: '/icon/wen2.png',
-      position: {
-        left: 270,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-      }, {
-        id: 13,
-        iconPath: '/icon/xin2.png',
-        position: {
-          left: 320,
-          top: 540 - 50,
-          width: 40,
-          height: 40
-        },
-        clickable: true
-    }, {
-      id: 1,
-      iconPath: '/icon/myloc.png',
-      position: {
-        left: 10,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-    }];
+    }
   },
-  buslineClickControls1: function () {
-    return [{
-      id: 11,
-      iconPath: '/icon/gong1.png',
+  leftTemplateFactory: function (H, W, id, iconPath, xoff) {
+    return {
+      id: id,
+      iconPath: iconPath,
       position: {
-        left: 220,
-        top: 540 - 50,
+        left: xoff,
+        top: H - 40 - 40 - 40,
         width: 40,
         height: 40
       },
       clickable: true
-    }, {
-      id: 12,
-      iconPath: '/icon/wen2.png',
-      position: {
-        left: 270,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-    }, {
-      id: 13,
-      iconPath: '/icon/xin2.png',
-      position: {
-        left: 320,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-    }, {
-      id: 1,
-      iconPath: '/icon/myloc.png',
-      position: {
-        left: 10,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-    }];
+    }
   },
-  buslineClickControls2: function () {
-    return [{
-      id: 11,
-      iconPath: '/icon/gong2.png',
-      position: {
-        left: 220,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-    }, {
-      id: 12,
-      iconPath: '/icon/wen1.png',
-      position: {
-        left: 270,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-    }, {
-      id: 13,
-      iconPath: '/icon/xin2.png',
-      position: {
-        left: 320,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-    }, {
-      id: 1,
-      iconPath: '/icon/myloc.png',
-      position: {
-        left: 10,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-    }];
+  buslineInitControls: function(H, W){
+    return [
+      this.rightTemplateFactory(H, W, 11, '/icon/gong2.png', (40 + 10) * 2),
+      this.rightTemplateFactory(H, W, 12, '/icon/wen2.png', (40 + 10) * 3),
+      this.rightTemplateFactory(H, W, 13, '/icon/xin2.png', (40 + 10) * 1),
+      this.leftTemplateFactory(H, W, 1, '/icon/myloc.png', 10),
+      ];
   },
-  buslineClickControls3: function () {
-    return [{
-      id: 11,
-      iconPath: '/icon/gong2.png',
-      position: {
-        left: 220,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-    }, {
-      id: 12,
-      iconPath: '/icon/wen2.png',
-      position: {
-        left: 270,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-    }, {
-      id: 13,
-      iconPath: '/icon/xin1.png',
-      position: {
-        left: 320,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-    }, {
-      id: 1,
-      iconPath: '/icon/myloc.png',
-      position: {
-        left: 10,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-    }];
+  buslineClickControls1: function (H, W) {
+    return [
+      this.rightTemplateFactory(H, W, 11, '/icon/gong1.png', (40 + 10) * 2),
+      this.rightTemplateFactory(H, W, 12, '/icon/wen2.png', (40 + 10) * 3),
+      this.rightTemplateFactory(H, W, 13, '/icon/xin2.png', (40 + 10) * 1),
+      this.leftTemplateFactory(H, W, 1, '/icon/myloc.png', 10)
+      ];
   },
-  leftInitControls: function(){
-    return [{
-      id: 1,
-      iconPath: '/icon/myloc.png',
-      position: {
-        left: 10,
-        top: 540 - 50,
-        width: 40,
-        height: 40
-      },
-      clickable: true
-    }]
+  buslineClickControls2: function (H, W) {
+    return [
+      this.rightTemplateFactory(H, W, 11, '/icon/gong2.png', (40 + 10) * 2),
+      this.rightTemplateFactory(H, W, 12, '/icon/wen1.png', (40 + 10) * 3),
+      this.rightTemplateFactory(H, W, 13, '/icon/xin2.png', (40 + 10) * 1),
+      this.leftTemplateFactory(H, W, 1, '/icon/myloc.png', 10)
+    ];
+  },
+  buslineClickControls3: function (H, W) {
+    return [
+      this.rightTemplateFactory(H, W, 11, '/icon/gong2.png', (40 + 10) * 2),
+      this.rightTemplateFactory(H, W, 12, '/icon/wen2.png', (40 + 10) * 3),
+      this.rightTemplateFactory(H, W, 13, '/icon/xin1.png', (40 + 10) * 1),
+      this.leftTemplateFactory(H, W, 1, '/icon/myloc.png', 10)
+    ];
+  },
+  leftInitControls: function(H, W){
+    return [this.leftTemplateFactory(H, W, 1, '/icon/myloc.png', 10)]
   },
 }
